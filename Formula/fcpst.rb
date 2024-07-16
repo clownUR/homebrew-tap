beb7242d9eaae1fce440441c9d70339b9ce2924d
clownUR <ruka.uesugi11055011@icloud.com>
2024-07-16 11:42:09 +0900

    fix invalid ruby code

--- Formula/fcpst.rb
+++ Formula/fcpst.rb
@@ -1,5 +1,6 @@
 VERSION="0.1.1"
 PRODUCT="fcpst"
+
 class Fcpst < Formula
   desc "A tool for archiving files and directories and extracting several archive formats."
   homepage "https://github.com/clownUR/fcpst"
@@ -7,18 +8,22 @@ class Fcpst < Formula
   license "MIT"
   option "without-completions", "Disable completions"
   depends_on "bash-completion@2" => :optional
+
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/clownUR/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_darwin_amd64.tar.gz"
     sha256 "f4ab387e2e03c559140a970497d90c0ae4e653fe80509647e7ce1b4df3ee3889"
   end
+
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/clownUR/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_darwin_arm64.tar.gz"
     sha256 "cb674e3facf3831a20cf1be9363f935f13205f604e672d4a8204228cd544fed4"
   end
+
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/clownUR/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_linux_amd64.tar.gz"
     sha256 "3be99051d6e2b039b0b91e47edba926676ea4b864727bf7eb960b88f8ec6affb"
   end
+
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/clownUR/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_linux_arm64.tar.gz"
     sha256 "334a660d3c5ab128d5f0712cacb60a032a85b1909949ba15fcf0a6f5023ea9d3"
@@ -33,5 +38,5 @@ class Fcpst < Formula
 
   test do
     system "#{bin}/fcpst --version"
-  done
+  end
 end
