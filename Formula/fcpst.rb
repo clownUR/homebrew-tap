9d2e161fa49b292403adb79e0d68fd3e9c8ca01b
clownUR <ruka.uesugi11055011@icloud.com>
2024-07-16 11:26:15 +0900

    fcpst.rbを修正しました

--- Formula/fcpst.rb
+++ Formula/fcpst.rb
@@ -1,4 +1,4 @@
-VERSION="0.1.0"
+VERSION="0.1.1"
 PRODUCT="fcpst"
 class Fcpst < Formula
   desc "A tool for archiving files and directories and extracting several archive formats."
@@ -9,19 +9,19 @@ class Fcpst < Formula
   depends_on "bash-completion@2" => :optional
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/clownUR/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_darwin_amd64.tar.gz"
-    sha256 "0fc84ded2d5a630c5d6c18781e96c03156e81e5be06a214cf018d46866bbe37c"
+    sha256 "f4ab387e2e03c559140a970497d90c0ae4e653fe80509647e7ce1b4df3ee3889"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/clownUR/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_darwin_arm64.tar.gz"
-    sha256 "231d31f06856146b6cee6829e577a8e89f178b522829aa725a3c6ad73b14dac0"
+    sha256 "cb674e3facf3831a20cf1be9363f935f13205f604e672d4a8204228cd544fed4"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/clownUR/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_linux_amd64.tar.gz"
-    sha256 "8a6ca11f56e2f24f5de0fd853d80e6e735bd76dd6985cecc6dd3257c1473f46a"
+    sha256 "3be99051d6e2b039b0b91e47edba926676ea4b864727bf7eb960b88f8ec6affb"
   end
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/clownUR/#{PRODUCT}/releases/download/v#{VERSION}/#{PRODUCT}-#{VERSION}_linux_arm64.tar.gz"
-    sha256 "8f84ea88797ad7e1b6f872be227211da2722c31c1a8b7acc80079028631c922c"
+    sha256 "334a660d3c5ab128d5f0712cacb60a032a85b1909949ba15fcf0a6f5023ea9d3"
 end
 end
 def install
